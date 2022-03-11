c7cf94e8a691b18f49dbff78ef29970e854a38c8
Sepehr Laal <sepehr.laal@gmail.com>
2022-03-10 22:48:04 -0800

    got everything to compile and not panic-crash with stacktrace in browser

--- scripts/replace-modules.sh
+++ scripts/replace-modules.sh
@@ -2,9 +2,9 @@
 
 root=$PWD
 pushd node_modules/terraform
-  go mod edit -replace github.com/chzyer/readline=$root/tfweb/readline
-  go mod edit -replace github.com/bgentry/speakeasy=$root/tfweb/speakeasy
-  go mod edit -replace github.com/armon/go-metrics=$root/tfweb/go-metrics
-  go mod edit -replace github.com/coreos/go-systemd=$root/tfweb/go-systemd
-  go mod edit -replace go.etcd.io/etcd=$root/tfweb/etcd
+  go mod edit -replace github.com/chzyer/readline=$root/readline
+  go mod edit -replace github.com/bgentry/speakeasy=$root/speakeasy
+  go mod edit -replace github.com/armon/go-metrics=$root/go-metrics
+  go mod edit -replace github.com/coreos/go-systemd=$root/go-systemd
+  go mod edit -replace go.etcd.io/etcd=$root/etcd
 popd
