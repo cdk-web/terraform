fcc5f9f861b6466808d4819c985c23915ffe94ef
Michael Neil <mneil@mneilsworld.com>
2022-03-13 17:27:03 -0700

    Refactor app into terraform class
    Set a work directory, user, home dir, and clean up the code
    a bit to expose terraform.

--- src/index.js
+++ src/index.js
@@ -1,31 +1,48 @@
 import { wbTfFs } from "./fs";
+import { Terraform } from "./terraform";
 // import for webpack?
-import * as exec from "./wasm_exec";
+
+const mainTf = `\
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
+`;
 
 const app = async () => {
-  const go = new Go();
+  const tf = new Terraform();
+  // write a tf file to plan
+  // wbTfFs.fs.writeFileSync(`${tf.workDir}/main.tf`, mainTf);
+  tf.load();
 
-  const response = await fetch("main.wasm");
-  const buffer = await response.arrayBuffer();
-  async function getWasm() {
-    return await WebAssembly.instantiate(buffer, go.importObject);
-  }
-  async function init() {
-    go.argv = ["terraform", "init"];
-    return go.run((await getWasm()).instance);
-  }
-  async function help() {
-    go.argv = ["js"]; // default
-    return go.run((await getWasm()).instance);
-  }
-
+  // build the DOM
   function component() {
     const initEl = document.createElement("button");
     initEl.innerHTML = "Terraform Init";
-    initEl.onclick = init;
+    initEl.onclick = tf.init.bind(tf);
     const helpEl = document.createElement("button");
     helpEl.innerHTML = "Terraform Help";
-    helpEl.onclick = help;
+    helpEl.onclick = tf.help.bind(tf);
     const app = document.createElement("div");
     const output = document.createElement("div");
 
---
+++ src/terraform.js
@@ -0,0 +1,52 @@
+// import to define Go
+import * as exec from "./wasm_exec";
+import { wbTfFs } from "./fs";
+
+export class Terraform extends Go {
+  wasm;
+  logLevel = "TRACE";
+  logPath = "/tmp/log.out";
+  workDir = "/app";
+  user = "terraform";
+  homeDir = "/home/terraform";
+
+  constructor() {
+    super();
+    this.env.USER = this.user;
+    this.env.HOME = this.homeDir;
+    this.env.TF_LOG = this.logLevel;
+    // setup logging for terraform
+    this.env.TF_LOG_PATH = this.logPath;
+    // create user home directory
+    wbTfFs.fs.mkdirSync(this.homeDir, { recursive: true });
+    // create log path directory
+    wbTfFs.fs.mkdirSync(this.logPath.split("/").slice(0, -1).join("/"), {
+      recursive: true,
+    });
+    // create app directory
+    wbTfFs.fs.mkdirSync(this.workDir, { recursive: true });
+    process.cwd = () => this.workDir;
+  }
+
+  async load() {
+    const response = await fetch("main.wasm");
+    this.wasm = await response.arrayBuffer();
+  }
+
+  async command(args) {
+    this.argv = ["terraform", ...args];
+    const res = await super.run(
+      (await WebAssembly.instantiate(this.wasm, this.importObject)).instance
+    );
+    // Debug log to console
+    console.log(fs.readFileSync(this.env.TF_LOG_PATH, { encoding: "utf8" }));
+    return res;
+  }
+
+  async init() {
+    return await this.command(["init"]);
+  }
+  async help() {
+    return await this.command([]);
+  }
+}
