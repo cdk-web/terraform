de48e11c899c63af6d8592645d6aaa03a4813320
Sepehr Laal <sepehr.laal@gmail.com>
2022-03-14 12:36:23 -0700

    better path handling in pure bash scripts, now they're directory-independent

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,6 +1,12 @@
 #! bash -eux
 
-pushd node_modules/terraform
-  GOOS=js GOARCH=wasm go build -o ../../public/main.wasm
-  cp "$(go env GOROOT)/misc/wasm/wasm_exec.js" ../../src
+__filename=$(readlink -f "$0")
+__dirname=$(dirname "$__filename")
+__root=$(dirname "$__dirname")
+
+pushd $__root
+  pushd node_modules/terraform
+    GOOS=js GOARCH=wasm go build -o ../../public/main.wasm
+    cp "$(go env GOROOT)/misc/wasm/wasm_exec.js" ../../src
+  popd
 popd
--- scripts/replace-modules.sh
+++ scripts/replace-modules.sh
@@ -1,10 +1,16 @@
 #! bash -eux
 
-root=$PWD
-pushd node_modules/terraform
-  go mod edit -replace github.com/chzyer/readline=$root/gomodules/readline
-  go mod edit -replace github.com/bgentry/speakeasy=$root/gomodules/speakeasy
-  go mod edit -replace github.com/armon/go-metrics=$root/gomodules/go-metrics
-  go mod edit -replace github.com/coreos/go-systemd=$root/gomodules/go-systemd
-  go mod edit -replace go.etcd.io/etcd=$root/gomodules/etcd
+__filename=$(readlink -f "$0")
+__dirname=$(dirname "$__filename")
+__root=$(dirname "$__dirname")
+
+pushd $__root
+  root=$PWD
+  pushd node_modules/terraform
+    go mod edit -replace github.com/chzyer/readline=$root/gomodules/readline
+    go mod edit -replace github.com/bgentry/speakeasy=$root/gomodules/speakeasy
+    go mod edit -replace github.com/armon/go-metrics=$root/gomodules/go-metrics
+    go mod edit -replace github.com/coreos/go-systemd=$root/gomodules/go-systemd
+    go mod edit -replace go.etcd.io/etcd=$root/gomodules/etcd
+  popd
 popd
