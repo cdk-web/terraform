f89ac637088eda862ed7856cbcd8af6eb3218419
Sepehr Laal <sepehr.laal@gmail.com>
2022-03-15 15:21:34 -0700

    expanded this script so it grabs more repos for us pre install

---
+++ scripts/get-git-deps.sh
@@ -0,0 +1,29 @@
+#! bash -eux
+
+__filename=$(readlink -f "$0")
+__dirname=$(dirname "$__filename")
+__root=$(dirname "$__dirname")
+
+installGitRepoAsNodeModule() {
+  name=$1
+  repo=$2
+  branch=$3
+  pushd $__root
+    rm -rf $name $name-src.tgz
+    git clone --depth=1 --branch ${branch} ${repo} $name
+    pushd $name
+      npm init -y
+      npm pack
+    popd
+    mv $name/$name-1.0.0.tgz $name-src.tgz && rm -rf $name
+  popd
+}
+
+dependencies=$(node -p "Object.keys(require('./package.json').gitDependencies).join('\n')")
+
+for dependency in $dependencies
+do
+  repo=$(node -p "require('./package.json').gitDependencies['${dependency}'].repo")
+  branch=$(node -p "require('./package.json').gitDependencies['${dependency}'].branch")
+  test -f $dependency-src.tgz || installGitRepoAsNodeModule $dependency $repo $branch
+done
