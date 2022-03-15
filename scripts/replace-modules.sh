ae25c260a22255afb1f9885524701d4a5994d914
Sepehr Laal <sepehr.laal@gmail.com>
2022-03-14 21:35:13 -0700

    make the terraform directory a git repo so we can see the diff in a git gui

--- scripts/replace-modules.sh
+++ scripts/replace-modules.sh
@@ -4,13 +4,13 @@ __filename=$(readlink -f "$0")
 __dirname=$(dirname "$__filename")
 __root=$(dirname "$__dirname")
 
-pushd $__root
-  root=$PWD
-  pushd node_modules/terraform
-    go mod edit -replace github.com/chzyer/readline=$root/gomodules/readline
-    go mod edit -replace github.com/bgentry/speakeasy=$root/gomodules/speakeasy
-    go mod edit -replace github.com/armon/go-metrics=$root/gomodules/go-metrics
-    go mod edit -replace github.com/coreos/go-systemd=$root/gomodules/go-systemd
-    go mod edit -replace go.etcd.io/etcd=$root/gomodules/etcd
-  popd
+pushd $__root/node_modules/terraform
+  go mod edit -replace github.com/chzyer/readline=$__root/gomodules/readline
+  go mod edit -replace github.com/bgentry/speakeasy=$__root/gomodules/speakeasy
+  go mod edit -replace github.com/armon/go-metrics=$__root/gomodules/go-metrics
+  go mod edit -replace github.com/coreos/go-systemd=$__root/gomodules/go-systemd
+  go mod edit -replace go.etcd.io/etcd=$__root/gomodules/etcd
+  git init
+  git add -A
+  git commit -m "unpatched"
 popd
