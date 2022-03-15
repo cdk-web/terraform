37b7f9c27e5cd59816f7a16448f0bc9985ddf80f
Sepehr Laal <sepehr.laal@gmail.com>
2022-03-15 12:49:57 -0700

    WIP: started messing with providers

--- scripts/replace-modules.sh
+++ scripts/replace-modules.sh
@@ -10,6 +10,24 @@ pushd $__root/node_modules/terraform
   go mod edit -replace github.com/armon/go-metrics=$__root/gomodules/go-metrics
   go mod edit -replace github.com/coreos/go-systemd=$__root/gomodules/go-systemd
   go mod edit -replace go.etcd.io/etcd=$__root/gomodules/etcd
+  pushd internal/command
+    mkdir -p bundled-providers
+    pushd bundled-providers
+      
+      git clone --depth 1 --branch v4.5.0 https://github.com/hashicorp/terraform-provider-aws.git
+      sed -i "1s/.*/module terraform-provider-aws/" terraform-provider-aws/go.mod
+      mv terraform-provider-aws/internal/* terraform-provider-aws
+      rm terraform-provider-aws/main.go
+
+      git clone --depth 1 --branch v2.2.2 https://github.com/hashicorp/terraform-provider-local.git
+      sed -i "1s/.*/module terraform-provider-local/" terraform-provider-local/go.mod
+      mv terraform-provider-local/internal/* terraform-provider-local
+      rm terraform-provider-local/main.go
+
+    popd
+  popd
+  go mod edit -replace terraform-provider-aws=$__root/node_modules/terraform/internal/command/bundled-providers/terraform-provider-aws
+  go mod edit -replace terraform-provider-local=$__root/node_modules/terraform/internal/command/bundled-providers/terraform-provider-local
   git init
   git add -A
   git commit -m "unpatched"
