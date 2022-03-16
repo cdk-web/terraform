cbe58d452fcf4ad28b6d5e9c450babc92ae49d42
Sepehr Laal <sepehr.laal@gmail.com>
2022-03-16 00:30:10 -0700

    added support for inlining git dependencies

--- scripts/get-git-deps.sh
+++ scripts/get-git-deps.sh
@@ -8,9 +8,16 @@ installGitRepoAsNodeModule() {
   name=$1
   repo=$2
   branch=$3
+  postCloneHook=$4
   pushd $__root
     rm -rf $name $name-src.tgz
     git clone --depth=1 --branch ${branch} ${repo} $name
+    if [ -n "$postCloneHook" ]
+    then
+      pushd $__root
+        bash -c "$postCloneHook"
+      popd
+    fi
     pushd $name
       npm init -y
       npm pack
@@ -25,5 +32,6 @@ for dependency in $dependencies
 do
   repo=$(node -p "require('$__root/package.json').gitDependencies['${dependency}'].repo")
   branch=$(node -p "require('$__root/package.json').gitDependencies['${dependency}'].branch")
-  test -f $dependency-src.tgz || installGitRepoAsNodeModule $dependency $repo $branch
+  postCloneHook=$(node -p "require('$__root/package.json').gitDependencies['${dependency}'].postClone||''")
+  test -f $dependency-src.tgz || installGitRepoAsNodeModule $dependency $repo $branch "$postCloneHook"
 done
